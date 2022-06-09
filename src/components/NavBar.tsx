64243e2e3f05a519cc3b7ca6e3675fac3fccc1f8
Astrid-Froitier <asfroitier@gmail.com>
2022-06-09 16:00:46 +0200

    update bugfix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ import navLinks_bottom from '../../data/navLinks_bottom';
 import navLinks_top from '../../data/navLinks_top';
 
 const NavBar = () => {
-  //   const { id, logout, admin } = useContext(CurrentUserContext);
+  // const { id, logout, admin } = useContext(CurrentUserContext);
   return (
     <div className="">
       <div className="alert">Message d&apos;alerte</div>
@@ -21,10 +21,19 @@ const NavBar = () => {
                 className="navBar__links__linksTop__a"
                 key={link.id}
                 to={link.path}>
-                <li className="navBar__links__linksTop__a__li">{link.title}</li>
+                <li className="navBar__links__linksTop__a__li">
+                  {link.title}
+                  {link.img}
+                </li>
               </NavLink>
             ))}
+            <img
+              width={20}
+              src="../../public/icone_connexion.png"
+              alt="icone's connexion"
+            />
           </div>
+
           <div className="navBar__links__linksBottom">
             {navLinks_bottom.map((link) => (
               <NavLink
@@ -36,46 +45,33 @@ const NavBar = () => {
             ))}
           </div>
         </div>
-
-        {/* <Link to="/">
-          <img width={200} src="../../pocli.png" alt="Pocli's logo" />
-        </Link>
       </div>
-      <div className="app__navBar">
+      {/* <div className="app__navBar">
         <nav className="app__navBar__nav">
           <div className="app__navBar__nav__linksTop">
-            <Link className="app__navBar__nav__linksTop__link" to="/about">
+            <NavLink className="app__navBar__nav__linksTop__link" to="/about">
               Qui sommes nous ?
-            </Link>
-            <Link className="app__navBar__nav__linksTop__link" to="/events">
+            </NavLink>
+            <NavLink className="app__navBar__nav__linksTop__link" to="/events">
               Évènements
-            </Link>
-            <Link className="app__navBar__nav__linksTop__link" to="/contact">
+            </NavLink>
+            <NavLink className="app__navBar__nav__linksTop__link" to="/contact">
               Contact
-            </Link> */}
-        {/* <Link className="app__navBar__nav__linksTop__link" to="/address">
-                  Adresses
-                </Link> */}
-        {/* <Link to="/click">Click</Link> */}
-        {/* {admin === true && <a href="http://localhost:3001/">Admin panel</a>}
+            </NavLink> */}
+      {/* <NavLink to="/click">Click</NavLink>
+            {admin === true && <a href="http://localhost:3001/">Admin panel</a>}
             {id === 0 ? (
-              <Link className="app__navBar__nav__linksTop__link" to="/login">
+              <NavLink className="app__navBar__nav__linksTop__link" to="/login">
                 Connexion
-              </Link>
+              </NavLink>
             ) : (
               <button className="logout" onClick={() => logout()}>
                 Se déconnecter
               </button>
             )}
-          </div>
-          <div>
-            <Link to="/family">Famille et parentalité</Link>
-            <Link to="/activities">Activités sportives et Bien-être</Link>
-            <Link to="/prevention">Prévention - Action Sociale</Link>
-            <Link to="/animation">Animation locale</Link>
-          </div>
-            </nav> */}
       </div>
+        </nav>
+      </div> */}
     </div>
   );
 };
