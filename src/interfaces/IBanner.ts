0c53a4770dad5e97f1e915781643251dd9db569b
fblaizoud <blaiz95@hotmail.com>
2022-06-17 15:17:09 +0200

    banner init for homepage

---
+++ src/interfaces/IBanner.ts
@@ -0,0 +1,9 @@
+export default interface IBanner {
+  activity: string;
+  title: string;
+  nameIcon: string;
+  bannerContact: boolean;
+  bannerEvent: boolean;
+  bannerMember: boolean;
+  memberFilter: boolean;
+}
