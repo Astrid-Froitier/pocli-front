fd4c83592c04b93ac7db352430129db226e6845c
Gavinus <96060707+Gavinus@users.noreply.github.com>
2022-06-20 14:53:23 +0200

    page Activities and Welfare finished

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -1,7 +1,114 @@
 import React from 'react';
+import Banner from './Banner';
+import Icon from './Icon';
+import ComeBackHome from './ComeBackHome';
 
 const Activities = () => {
-  return <div className="">Activités sportives et Bien-être</div>;
+  return (
+    <div className="">
+      <Banner
+        nameBannerActivity="activity sport"
+        title="Activités sportives et de Bien-Être"
+        nameIcon="person-running"
+        memberFilter={false}
+        bannerAbout={false}
+        bannerEvent={false}
+        bannerMember={false}
+      />
+      <div className="activityContainer">
+        <div className="activityContainer__preambulContainer">
+          <h2>Un lieu d’épanouissement individuel et collectif.</h2>
+          <p>
+            Vous pratiquerez des activités sportives dans une ambiance détendue et
+            conviviale : pilates, gym douce, gym seniors. Vous renforcerez vos relations
+            avec vos enfants lors de séances de bien être duo parents/enfants :
+            méditation, yoga, massage, balade sensorielle. Vous prendrez du temps pour
+            vous : activités et sorties bien-être solo (méditation, yoga), soirées papote
+            autour d’un verre, …
+          </p>
+        </div>
+        <div className="activityContainer__pilateContainer">
+          <h1>Pilates</h1>
+          <div className="activityContainer__pilateContainer__pilateContainerBloc1">
+            <Icon name="arrow-right" width="50px" height="40px" color="#AF3D3D" />
+            <p>Renforcement musculaire en douceur allié à une respiration profonde.</p>
+          </div>
+          <div className="activityContainer__pilateContainer__pilateContainerBloc2">
+            <Icon name="arrow-right" width="50px" height="40px" color="#AF3D3D" />
+            <p>
+              Travail essentiellement au sol, avec ou sans matériel (élastiques, ballon…).
+            </p>
+          </div>
+          <div className="activityContainer__pilateContainer__pilateContainerHour">
+            <p>Séances le lundi de 19h à 20h.</p>
+            <p>Salle des fêtes d’Espiet.</p>
+          </div>
+        </div>
+        <div className="activityContainer__gymSeniorContainer">
+          <h1>Gym Seniors</h1>
+          <div className="activityContainer__gymSeniorContainer__gymSeniorContainerBloc1">
+            <Icon name="arrow-right" width="50px" height="40px" color="#AF3D3D" />
+            <p>
+              Renforcement musculaire, équilibre dynamique, stimulation de la fonction
+              cardia-respiratoire, renforcement abdos fessiers, assouplissement de la
+              colonne vertébrale, étirement de la chaîne musculaire.
+            </p>
+          </div>
+          <div className="activityContainer__gymSeniorContainer__gymSeniorHour">
+            <p>Séances le mardi de 11h15 à 12h15.</p>
+            <p>Salle des fêtes de St Quentin de Baron.</p>
+          </div>
+        </div>
+        <div className="activityContainer__gymSoftContainer">
+          <h1>Gym Douce</h1>
+          <div className="activityContainer__gymSoftContainer__gymSoftContainerBloc1">
+            <Icon name="arrow-right" width="50px" height="40px" color="#AF3D3D" />
+            <p>
+              Travail de respiration, équilibre dynamique, stretching, sens du rythme,
+              coordination, relaxation.
+            </p>
+          </div>
+          <div className="activityContainer__gymSoftContainer__gymSoftHour">
+            <p>Séances le mardi de 11h15 à 12h15.</p>
+            <p>Salle des fêtes de St Quentin de Baron.</p>
+          </div>
+        </div>
+        <div className="activityContainer__welfareContainer">
+          <h1>Bien-Être Solo</h1>
+          <div className="activityContainer__welfareContainer__welfareContainerBloc1">
+            <Icon name="arrow-right" width="50px" height="40px" color="#AF3D3D" />
+            <p>Cycles de découvertes d’activités relaxantes : méditation, yoga.</p>
+          </div>
+          <div className="activityContainer__welfareContainer__welfareContainerBloc2">
+            <Icon name="arrow-right" width="50px" height="40px" color="#AF3D3D" />
+            <p>Sorties bien-être : Calicéo, balade nature, …</p>
+          </div>
+        </div>
+        <div className="activityContainer__welfareDuoContainer">
+          <h1>Bien-Être Duo</h1>
+          <div className="activityContainer__welfareDuoContainer__welfareDuoContainerBloc1">
+            <Icon name="arrow-right" width="50px" height="40px" color="#AF3D3D" />
+            <p>Partager un moment de bien être avec son enfant.</p>
+          </div>
+          <div className="activityContainer__welfareDuoContainer__welfareDuoContainerBloc1">
+            <Icon name="arrow-right" width="50px" height="40px" color="#AF3D3D" />
+            <p>Massage bébé, yoga famille, méditation, sortie nature, …</p>
+          </div>
+        </div>
+        <div className="activityContainer__activityPictures">
+          <div className="activityContainer__activityPictures__activityPicturesBloc1">
+            <img src="/assets/bienetresolo.png" alt="SportThemeImage1" />
+            <img src="/assets/bienetreduo.png" alt="SportThemeImage2" />
+          </div>
+          <div className="activityContainer__activityPictures__activityPicturesBloc2">
+            <img src="/assets/eventexample.jpg" alt="SportThemeImage3" />
+            <img src="/assets/pilate.png" alt="SportThemeImage4" />
+          </div>
+        </div>
+      </div>
+      <ComeBackHome />
+    </div>
+  );
 };
 
 export default Activities;
