b51eed2a524411dd091fd54613b734c8c2275312
fblaizoud <blaiz95@hotmail.com>
2022-07-15 15:47:56 +0200

    fix bugs

--- src/components/AdherentSpace.tsx
+++ src/components/AdherentSpace.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getAllDataWithoutCredential } from '../../helpers/axios';
 import CurrentUserContext from '../contexts/CurrentUser';
 import Banner from './Banner';
